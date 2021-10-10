d65f268e9182718836f929ec2e7aff64ecb1a268
Nikolai Varlamov <varlamow.col@yahoo.com>
2021-10-11 00:21:25 +0300

    database config update

--- modules/database/database.go
+++ modules/database/database.go
@@ -9,35 +9,45 @@ import (
 	"gorm.io/gorm"
 )
 
-func New() (*gorm.DB, error) {
-	return gorm.Open(postgres.Open(dsn), &gorm.Config{})
+var connectionString string
+
+func Init() {
+	fmt.Println("db init!")
+	connectionString = makeConnectionString()
 }
 
-func getConnectionString() string {
+func New() (*gorm.DB, error) {
+	return gorm.Open(postgres.Open(connectionString), &gorm.Config{})
+}
+
+func makeConnectionString() string {
 	var err error
 
 	host := ""
 	if host, err = utils.GetEnvStr("DB_HOST"); err != nil {
+		fmt.Println("here 1")
 		host = viper.GetString("database.host")
+		fmt.Println("host: ", host)
 	}
 
-	port := 5442
-	if port, err = utils.GetEnvInt("DB_HOST"); err != nil {
+	port := 0
+	if port, err = utils.GetEnvInt("DB_PORT"); err != nil {
+		fmt.Println("here 2")
 		port = viper.GetInt("database.port")
 	}
 
 	username := ""
-	if username, err = utils.GetEnvStr("DB_HOST"); err != nil {
+	if username, err = utils.GetEnvStr("DB_USERNAME"); err != nil {
 		username = viper.GetString("database.username")
 	}
 
 	password := ""
-	if password, err = utils.GetEnvStr("DB_HOST"); err != nil {
+	if password, err = utils.GetEnvStr("DB_PASSWORD"); err != nil {
 		password = viper.GetString("database.password")
 	}
 
 	db := ""
-	if db, err = utils.GetEnvStr("DB_HOST"); err != nil {
+	if db, err = utils.GetEnvStr("DB_DB"); err != nil {
 		db = viper.GetString("database.db")
 	}
 
